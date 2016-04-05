46a36a1a4a24088ad7e0d395c8508a704efd4db7
George-Kagwe <georgegkagwe@gmail.com>
2016-04-05 12:29:36 +0300

    Tables array

--- application/controllers/synchronization.php
+++ application/controllers/synchronization.php
@@ -43,6 +43,101 @@ class Synchronization extends MY_Controller {
 		$template = 'shared_files/template/template';
 		$this -> load -> view($template, $data);
 
+	} 
+
+
+	public function  sync(){
+    
+     // Array with all the tables  // hard coded but am sure there is another way in codeigniter
+
+
+       /*$all_tables = array( 'access_level ' ,                      
+			 'account_list',                       
+			 'assignments' ,                        
+			 'comments '  ,                         
+			 'commodities' ,                        
+			 'commodity_category' ,                 
+			 'commodity_division_details ',        
+			 'commodity_source'    ,                
+			 'commodity_source_other',              
+			 'commodity_source_other_prices' ,      
+			 'commodity_source_sub_category',       
+			 'commodity_sub_category'   ,           
+			' counties'      ,                      
+			 'county_drug_store_issues' ,           
+			 'county_drug_store_totals'  ,          
+			 'county_drug_store_transaction_table',
+			 'dispensing_records'        ,          
+			 'dispensing_stock_prices',             
+			 'dispensing_totals'  ,                 
+			 'districts'        ,                   
+			 'drug_commodity_map',                  
+			 'drug_store_issues' ,                  
+			 'drug_store_totals',                   
+			 'drug_store_transaction_table',        
+			 'email_listing'      ,                 
+			 'facilities'  ,                        
+			 'facility_amc',                        
+			 'facility_amc1'  ,                     
+			 'facility_evaluation' ,                
+			 'facility_impact_evaluation'  ,        
+			 'facility_issues'    ,                 
+			 'facility_monthly_stock',              
+			'facility_order_details' ,             
+			 'facility_order_details_rejects',      
+			 'facility_order_status'  ,             
+			 'facility_orders'   ,                  
+			 'facility_stock_out_tracker'  ,        
+			 'facility_stock_status' ,              
+			 'facility_stocks'   ,                  
+			 'facility_stocks_temp',                
+			 'facility_transaction_table',          
+			 'facility_user_log'  ,                 
+			 'issue_type' ,                         
+			 'log' ,                                
+			 'log_monitor',                         
+			 'malaria_data'  ,                      
+			 'malaria_drugs' ,                      
+			 'menu' ,                               
+			 'messages' ,                           
+			 'patient_details' ,                    
+			 'patient_issues',                      
+			 'patients'    ,                        
+			 'project_teams',                       
+			 'projects' ,                           
+			 'rca_county',                          
+			'recepients' ,                         
+			 'redistribution_data' ,                
+			 'requisitions' ,                       
+			 'reversals'   ,                        
+			 'rh_drugs_data',                       
+			 'service_point_stocks' ,               
+			 'service_points' ,                     
+			 'source_facility',                     
+			 'status'   ,                           
+			 'sub_menu',                            
+			 'tasks'  ,                             
+			'team_positions' ,                     
+			 'tuberculosis_data' ,                  
+			'user'   );
+              */
+			
+           //access all the tables in the array
+           $all_tables = $this->db->list_tables();
+          
+          foreach ($all_tables as $key => $value) {
+          	
+          	 echo "$value" ."<br/>";
+           
+
+           //$data_from_table = Sync_Model::get_all($value);
+
+ 
+
+          }
+
+
+
 	}
 }
 
